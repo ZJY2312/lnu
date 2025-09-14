d4907daf77fa883f364bc3a1cdb7e1584fec28d1
ZJY2312 <2312003686@qq.com>
2025-09-14 08:49:38 +0800

    Update generate_script.py

--- generate_script.py
+++ generate_script.py
@@ -7,13 +7,22 @@ with open('configs/config.json', 'r') as f:
 
 # 加载模板
 env = Environment(loader=FileSystemLoader('templates'))
-template = env.get_template('script_template.py.j2')
+template = env.get_template('script_template1.py.j2')
 
 # 渲染模板
 rendered = template.render(config)
 
 # 写入生成的脚本
-with open('script.py', 'w') as f:
+with open('script1.py', 'w') as f:
     f.write(rendered)
 
 print("✅ script.py 已生成")
+
+template = env.get_template('script_template2.py.j2')
+
+# 渲染模板
+rendered = template.render(config)
+
+# 写入生成的脚本
+with open('script2.py', 'w') as f:
+    f.write(rendered)
